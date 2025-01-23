41d0b2e3ae6142e6fdf2d41b89827c918796148e
Juhyeong Ko <dury.ko@gmail.com>
2025-01-23 15:07:21 +0900

    ch01: switch문 함수로 추출
    코드 분석으로 얻은 정보는 휘발성이 높아서 재빨리 코드에 반영하는 것이 좋다

--- src/main/kotlin/chapter01/Statement.kt
+++ src/main/kotlin/chapter01/Statement.kt
@@ -4,22 +4,16 @@ import java.text.NumberFormat
 import java.util.Locale.US
 
 fun statement(invoice: Invoice, plays: Plays): String {
-    var totalAmount = 0
-    var volumeCredit = 0
-    var result = "청구 내역 (고객명: ${invoice.customer})\n"
-    val format = { number: Double -> NumberFormat.getCurrencyInstance(US).format(number) }
-
-    for (perf in invoice.performances) {
-        val play = plays[perf.playID]
+    fun amountFor(perf: Performance, play: Play): Int {
         var thisAmount = 0
-
-        when (play!!.type) {
+        when (play.type) {
             "tragedy" -> {
                 thisAmount = 40000
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30)
                 }
             }
+
             "comedy" -> {
                 thisAmount = 30000
                 if (perf.audience > 20) {
@@ -27,13 +21,27 @@ fun statement(invoice: Invoice, plays: Plays): String {
                 }
                 thisAmount += 300 * perf.audience
             }
+
             else -> {
                 throw Error("알 수 없는 장르: ${play.type}")
             }
         }
 
+        return thisAmount
+    }
+
+    var totalAmount = 0
+    var volumeCredit = 0
+    var result = "청구 내역 (고객명: ${invoice.customer})\n"
+    val format = { number: Double -> NumberFormat.getCurrencyInstance(US).format(number) }
+
+    for (perf in invoice.performances) {
+        val play = plays[perf.playID]!!
+        val thisAmount = amountFor(perf, play)
+
         // 포인트를 적립한다.
         volumeCredit += maxOf(perf.audience - 30, 0)
+
         // 희극 관객 5명마다 추가 포인트를 제공한다
         if (play.type == "comedy") volumeCredit += perf.audience / 5
 
@@ -42,7 +50,7 @@ fun statement(invoice: Invoice, plays: Plays): String {
         totalAmount += thisAmount
     }
 
-    result += "총액: \$${"%,.2f".format(totalAmount / 100.0)}\n"
+    result += "총액: $${"%,.2f".format(totalAmount / 100.0)}\n"
     result += "적립 포인트: ${volumeCredit}점"
     return result
 }

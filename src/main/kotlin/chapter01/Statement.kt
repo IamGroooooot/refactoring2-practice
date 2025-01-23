33ad92340559edd9b28203bc6eeac812729fd2a0
Juhyeong Ko <dury.ko@gmail.com>
2025-01-23 17:57:20 +0900

    ch01: statement에 필요한 데이터 처리를 하는 함수 추출

--- src/main/kotlin/chapter01/Statement.kt
+++ src/main/kotlin/chapter01/Statement.kt
@@ -3,79 +3,8 @@ package chapter01
 import java.text.NumberFormat
 import java.util.Locale.US
 
-data class StatementData(
-    val customer: String, val performances: List<EnrichedPerformance>
-) {
-    var totalAmount: Int = 0
-    var totalVolumeCredits: Int = 0
-}
-
-class EnrichedPerformance(
-    val playID: String, val audience: Int, val play: Play
-) {
-    var amount: Int = 0
-    var volumeCredits: Int = 0
-}
-
 fun statement(invoice: Invoice, plays: Plays): String {
-    fun playFor(aPerformance: Performance): Play {
-        return plays[aPerformance.playID]!!
-    }
-
-    fun amountFor(aPerformance: EnrichedPerformance): Int {
-        var result = 0
-        when (aPerformance.play.type) {
-            "tragedy" -> {
-                result = 40000
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-            }
-
-            "comedy" -> {
-                result = 30000
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-            }
-
-            else -> {
-                throw Error("알 수 없는 장르: ${aPerformance.play.type}")
-            }
-        }
-
-        return result
-    }
-
-    fun volumeCreditsFor(aPerformance: EnrichedPerformance): Int {
-        var result = maxOf(aPerformance.audience - 30, 0)
-        if (aPerformance.play.type == "comedy") result += aPerformance.audience / 5
-        return result
-    }
-
-    fun totalVolumeCredits(data: StatementData): Int {
-        return data.performances.sumOf { it.volumeCredits }
-    }
-
-    fun totalAmount(data: StatementData): Int {
-        return data.performances.sumOf { it.amount }
-    }
-
-    fun enrichPerformance(aPerformance: Performance): EnrichedPerformance {
-        val result = EnrichedPerformance(
-            aPerformance.playID, aPerformance.audience, playFor(aPerformance)
-        )
-        result.amount = amountFor(result)
-        result.volumeCredits = volumeCreditsFor(result)
-        return result
-    }
-
-    val statementData = StatementData(invoice.customer, invoice.performances.map { enrichPerformance(it) }).apply {
-        totalAmount = totalAmount(this)
-        totalVolumeCredits = totalVolumeCredits(this)
-    }
-    return renderPlainText(statementData, plays)
+    return renderPlainText(createStatementData(plays, invoice), plays)
 }
 
 private fun renderPlainText(data: StatementData, plays: Plays): String {

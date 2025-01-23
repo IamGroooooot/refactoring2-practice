a3de331c6f16315d8ed060b5d280bb0abf989376
Juhyeong Ko <dury.ko@gmail.com>
2025-01-23 18:02:59 +0900

    ch01: renderHtml 추가

--- src/main/kotlin/chapter01/Statement.kt
+++ src/main/kotlin/chapter01/Statement.kt
@@ -7,11 +7,32 @@ fun statement(invoice: Invoice, plays: Plays): String {
     return renderPlainText(createStatementData(plays, invoice), plays)
 }
 
-private fun renderPlainText(data: StatementData, plays: Plays): String {
+fun htmlStatement(invoice: Invoice, plays: Plays): String {
+    return renderHtml(createStatementData(plays, invoice))
+}
+
+fun renderHtml(data: StatementData): String {
     fun usd(number: Int): String {
         return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
     }
 
+    var result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>"
+    for (perf in data.performances) {
+        result += "<tr><td>${perf.play.name}</td><td>${perf.audience}</td>"
+        result += "<td>${usd(perf.amount)}</td></tr>\n"
+    }
+    result += "</table>\n"
+    result += "<p>총액: <em>${usd(data.totalAmount)}</em></p>\n"
+    result += "<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>"
+    return result
+}
+
+private fun renderPlainText(data: StatementData, plays: Plays): String {
+    fun usd(number: Int): String {
+        return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
+    }
 
     var result = "청구 내역 (고객명: ${data.customer})\n"
     for (perf in data.performances) {

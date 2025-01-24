9bfa69bbacf882c715da3333e7bee3e965ea96cf
Juhyeong Ko <dury.ko@gmail.com>
2025-01-24 20:26:42 +0900

    ch01: usd 함수 추출

--- src/main/kotlin/chapter01/Statement.kt
+++ src/main/kotlin/chapter01/Statement.kt
@@ -3,8 +3,23 @@ package chapter01
 import java.text.NumberFormat
 import java.util.Locale.US
 
+fun usd(number: Int): String {
+    return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
+}
+
 fun statement(invoice: Invoice, plays: Plays): String {
     return renderPlainText(createStatementData(plays, invoice), plays)
+}
+
+private fun renderPlainText(data: StatementData, plays: Plays): String {
+    var result = "청구 내역 (고객명: ${data.customer})\n"
+    for (perf in data.performances) {
+        // 청구 내역을 출력한다.
+        result += "  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n"
+    }
+    result += "총액: ${usd(data.totalAmount)}\n"
+    result += "적립 포인트: ${data.totalVolumeCredits}점"
+    return result
 }
 
 fun htmlStatement(invoice: Invoice, plays: Plays): String {
@@ -12,10 +27,6 @@ fun htmlStatement(invoice: Invoice, plays: Plays): String {
 }
 
 fun renderHtml(data: StatementData): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
-    }
-
     var result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n"
     result += "<table>\n"
     result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n"
@@ -26,20 +37,5 @@ fun renderHtml(data: StatementData): String {
     result += "</table>\n"
     result += "<p>총액: <em>${usd(data.totalAmount)}</em></p>\n"
     result += "<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>"
-    return result
-}
-
-private fun renderPlainText(data: StatementData, plays: Plays): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
-    }
-
-    var result = "청구 내역 (고객명: ${data.customer})\n"
-    for (perf in data.performances) {
-        // 청구 내역을 출력한다.
-        result += "  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n"
-    }
-    result += "총액: ${usd(data.totalAmount)}\n"
-    result += "적립 포인트: ${data.totalVolumeCredits}점"
     return result
 }

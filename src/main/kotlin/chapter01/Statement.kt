e1566ae833ad0384b9ffef3d3ee375ec34e25e85
Juhyeong Ko <dury.ko@gmail.com>
2025-01-24 22:36:42 +0900

    ch01: usd 함수 추출 재적용

--- src/main/kotlin/chapter01/Statement.kt
+++ src/main/kotlin/chapter01/Statement.kt
@@ -3,15 +3,15 @@ package chapter01
 import java.text.NumberFormat
 import java.util.Locale.US
 
+fun usd(number: Int): String {
+    return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
+}
+
 fun statement(invoice: Invoice, plays: Plays): String {
     return renderPlainText(createStatementData(plays, invoice), plays)
 }
 
 private fun renderPlainText(data: StatementData, plays: Plays): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
-    }
-
     var result = "청구 내역 (고객명: ${data.customer})\n"
     for (perf in data.performances) {
         // 청구 내역을 출력한다.
@@ -27,10 +27,6 @@ fun htmlStatement(invoice: Invoice, plays: Plays): String {
 }
 
 fun renderHtml(data: StatementData): String {
-    fun usd(number: Int): String {
-        return NumberFormat.getCurrencyInstance(US).format(number / 100.0)
-    }
-
     var result = "<h1>청구 내역 (고객명: ${data.customer})</h1>\n"
     result += "<table>\n"
     result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n"
